5e4584fd6c42e76731a3f1f9ef7cabcabd171fe8
elvis <elv.velasquez@duocuc.cl>
2023-06-03 10:42:35 -0400

    agregando BD

--- miDjango/settings.py
+++ miDjango/settings.py
@@ -74,11 +74,26 @@ WSGI_APPLICATION = 'miDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME':'127.0.0.1:1521/orcl',
+        'USER' : 'c##prueba',
+        'PASSWORD': 'prueba',
+        'TEST' :{
+            'USER': 'defaul_test',
+            'TBLSPACE':'default_test_tbls',
+            'TBL_SPACE_TMP':'defaul_test_tbls_tmp',
+        },
+    },
 }
 
 
